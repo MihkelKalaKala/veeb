3a91ce8b6e843c5cd82c571f59148e98cb9de125
MihkelKalaKala <MihkelKala@aol.com>
2016-06-15 08:06:07 +0300

    pisimuudatused
    autocomplete välja

--- muuda.php
+++ muuda.php
@@ -15,18 +15,18 @@
 			<fieldset>
 				<legend>Isiklik informatsioon:</legend>
                 Eesnimi:<br>
-				<input type="text" name="enimi" value="<?php print $andmed["enimi"]; ?>" maxlength="30" required><br>
+				<input type="text" name="enimi" value="<?php print $andmed["enimi"]; ?>" maxlength="30" autocomplete="off" required><br>
 				Perenimi:<br>
-				<input type="text" name="pnimi" value="<?php print $andmed["pnimi"]; ?>" maxlength="30" required><br>
+				<input type="text" name="pnimi" value="<?php print $andmed["pnimi"]; ?>" maxlength="30" autocomplete="off" required><br>
 				<br>
 				Sugu:<br>
 				<input value="Mees" type="radio" name="sugu" <?php print ($andmed["sugu"] == "Mees" ? "checked" : "");?> >Mees<br>
                 <input value="Naine" type="radio" name="sugu" <?php print ($andmed["sugu"] == "Naine" ? "checked" : "");?> >Naine<br>
 				<br>
                 Telefon:<br>
-				<input type="number" name="tel" value="<?php print $andmed["tel"]; ?>" maxlength="20" required><br>
+				<input type="number" name="tel" value="<?php print $andmed["tel"]; ?>" maxlength="20" autocomplete="off" required><br>
                 E-Mail:<br>
-				<input type="email" name="epost" value="<?php print $andmed["epost"]; ?>" maxlength="100" required><br>
+				<input type="email" name="epost" value="<?php print $andmed["epost"]; ?>" maxlength="100" autocomplete="off" required><br>
 				<br>
                 Pilt:<br>
                 <img src="<?php print "./andmebaas/$isik/pilt.jpg"; ?>" height="150" width="150"><br><br>
